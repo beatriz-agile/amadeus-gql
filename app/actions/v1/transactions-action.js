5d3d8ab8dbdc3794687e69757920df222d4f7943
Beatriz Ramos Rodrigues <beatriz.rodrigues@agilecontent.com>
2019-06-26 04:52:04 +0000

    Updated package.json and added action

---
+++ app/actions/v1/transactions-action.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+class TransactionsAction {
+  static async getTransactions(context) {
+    const response = context.response();
+    const amadeusService = context.shared.get('amadeusService');
+
+    const transactionsResult = await amadeusService.getTransactionsByCustomer(context, parameters.instanceId,
+      parameters.customerId, parameters.query, parameters.fields, parameters.offset, parameters.limit);
+
+    if (!transactionsResult.isSuccess) {
+      return serviceError(serviceResult, response);
+    }
+    
+    const body = transactionsResult.body;
+    const result = body && body.result ? body.result : {};
+    return response.result(result).end();
+  }
+}
+
+module.exports = TransactionsAction;
